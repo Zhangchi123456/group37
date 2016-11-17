074999417eea168e7e72a4e714b74822a7afb1dd
John <zhengzonghong@outlook.com>
2016-11-17 18:49:01 +0800

    try to push

--- Hotel-parent/Hotel-client/src/main/java/org/Hotel/client/App.java
+++ Hotel-parent/Hotel-client/src/main/java/org/Hotel/client/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-    	System.out.print("hello people");
+    	System.out.println("arg0");
+    	System.out.println("hello people");
         System.out.println( "Hello World!" );
     }
 }
